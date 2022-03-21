930bb14df980d9b3c391aff56f0704ec9c0513ef
Lech Piotrowski <science@lwp.email>
2022-03-21 18:43:36 +0100

    Now EventVoltage has all the field of EventADC, but some are float

--- GRANDEventVoltage.cpp
+++ GRANDEventVoltage.cpp
@@ -66,9 +66,9 @@ GRANDEventVoltage::GRANDEventVoltage(GRANDEventADC *adc) : GRANDEventVoltage()
 void GRANDEventVoltage::ADCs2Real(GRANDEventADC *adc)
 {
 	// Clear the traces vectors
-	x.clear();
-	y.clear();
-	z.clear();
+	trace_x.clear();
+	trace_y.clear();
+	trace_z.clear();
 
 	// Clear the GPS vectors
 	gps_long.clear();
@@ -80,9 +80,9 @@ void GRANDEventVoltage::ADCs2Real(GRANDEventADC *adc)
 	for (size_t i=0; i<adc->du_count; ++i)
 	{
 		// Create this DU's vectors
-		x.push_back(vector<float>());
-		y.push_back(vector<float>());
-		z.push_back(vector<float>());
+		trace_x.push_back(vector<float>());
+		trace_y.push_back(vector<float>());
+		trace_z.push_back(vector<float>());
 		// Convert this specific DU's ADCs to Voltage
 		TraceADC2Voltage(i, adc);
 		// Convert GPS ADC to real values
@@ -96,12 +96,12 @@ void GRANDEventVoltage::TraceADC2Voltage(int du_num, GRANDEventADC *adc)
 	// Also, at the moment I assume trace_0/1/2 are x/y/z - this may also change in the future
 	// The conversion factor is just taken from the information for XiHu data, that "For currently ADC, the differential input voltage range is 1.8V (Vpp), that is -0.9V to 0.9V corresponding to ADC value -8192 to 8192"
 	float adc2voltageconst=0.9/8192;
-	x[du_num].resize(adc->trace_0[du_num].size());
-	transform(adc->trace_0[du_num].begin(), adc->trace_0[du_num].end(), x[du_num].begin(), [adc2voltageconst](short &c){ return c*adc2voltageconst; });
-	y[du_num].resize(adc->trace_1[du_num].size());
-	transform(adc->trace_1[du_num].begin(), adc->trace_1[du_num].end(), y[du_num].begin(), [adc2voltageconst](short &c){ return c*adc2voltageconst; });
-	z[du_num].resize(adc->trace_2[du_num].size());
-	transform(adc->trace_2[du_num].begin(), adc->trace_2[du_num].end(), z[du_num].begin(), [adc2voltageconst](short &c){ return c*adc2voltageconst; });
+	trace_x[du_num].resize(adc->trace_0[du_num].size());
+	transform(adc->trace_0[du_num].begin(), adc->trace_0[du_num].end(), trace_x[du_num].begin(), [adc2voltageconst](short &c){ return c*adc2voltageconst; });
+	trace_y[du_num].resize(adc->trace_1[du_num].size());
+	transform(adc->trace_1[du_num].begin(), adc->trace_1[du_num].end(), trace_y[du_num].begin(), [adc2voltageconst](short &c){ return c*adc2voltageconst; });
+	trace_z[du_num].resize(adc->trace_2[du_num].size());
+	transform(adc->trace_2[du_num].begin(), adc->trace_2[du_num].end(), trace_z[du_num].begin(), [adc2voltageconst](short &c){ return c*adc2voltageconst; });
 }
 
 void GRANDEventVoltage::GPSADC2Real(int du_num, GRANDEventADC *adc)
@@ -117,24 +117,21 @@ TTree *GRANDEventVoltage::CreateTree()
 	teventvoltage = new TTree("teventvoltage", "Event with Voltage traces and other information");
 
 	// Create Branches for all stored data
-	// Int branches
-//	teventvoltage->Branch("event_size", &event_size, "event_size/i");
+	teventvoltage->Branch("event_size", &event_size, "event_size/i");
 	teventvoltage->Branch("run_number", &run_number, "run_number/i");
 	teventvoltage->Branch("event_number", &event_number, "event_number/i");
 	teventvoltage->Branch("t3_number", &t3_number, "t3_number/i");
 	teventvoltage->Branch("first_du", &first_du, "first_du/i");
 	teventvoltage->Branch("time_seconds", &time_seconds, "time_seconds/i");
 	teventvoltage->Branch("time_nanoseconds", &time_nanoseconds, "time_nanoseconds/i");
-//	teventvoltage->Branch("time", &time, "time/D");
 	teventvoltage->Branch("event_type", &event_type, "event_type/i");
 	teventvoltage->Branch("event_version", &event_version, "event_version/i");
 	teventvoltage->Branch("du_count", &du_count, "du_count/i");
 	// Vector branches
-//	teventvoltage->Branch("event_id", &event_id);
+	teventvoltage->Branch("event_id", &event_id);
 	teventvoltage->Branch("du_id", &du_id);
 	teventvoltage->Branch("du_seconds", &du_seconds);
 	teventvoltage->Branch("du_nanoseconds", &du_nanoseconds);
-//	teventvoltage->Branch("du_time", &du_time);
 	teventvoltage->Branch("trigger_position", &trigger_position);
 	teventvoltage->Branch("trigger_flag", &trigger_flag);
 	teventvoltage->Branch("atm_temperature", &atm_temperature);
@@ -143,45 +140,45 @@ TTree *GRANDEventVoltage::CreateTree()
 	teventvoltage->Branch("acceleration_x", &acceleration_x);
 	teventvoltage->Branch("acceleration_y", &acceleration_y);
 	teventvoltage->Branch("acceleration_z", &acceleration_z);
-//	teventvoltage->Branch("battery_level", &battery_level);
+	teventvoltage->Branch("battery_level", &battery_level);
 	teventvoltage->Branch("firmware_version", &firmware_version);
-//	teventvoltage->Branch("adc_sampling_frequency", &adc_sampling_frequency);
-//	teventvoltage->Branch("adc_sampling_resolution", &adc_sampling_resolution);
-//	teventvoltage->Branch("adc_input_channels", &adc_input_channels);
-//	teventvoltage->Branch("adc_enabled_channels", &adc_enabled_channels);
-//	teventvoltage->Branch("adc_samples_count_total", &adc_samples_count_total);
-//	teventvoltage->Branch("adc_samples_count_channel0", &adc_samples_count_channel0);
-//	teventvoltage->Branch("adc_samples_count_channel1", &adc_samples_count_channel1);
-//	teventvoltage->Branch("adc_samples_count_channel2", &adc_samples_count_channel2);
-//	teventvoltage->Branch("adc_samples_count_channel3", &adc_samples_count_channel3);
+	teventvoltage->Branch("adc_sampling_frequency", &adc_sampling_frequency);
+	teventvoltage->Branch("adc_sampling_resolution", &adc_sampling_resolution);
+	teventvoltage->Branch("adc_input_channels", &adc_input_channels);
+	teventvoltage->Branch("adc_enabled_channels", &adc_enabled_channels);
+	teventvoltage->Branch("adc_samples_count_total", &adc_samples_count_total);
+	teventvoltage->Branch("adc_samples_count_channel0", &adc_samples_count_channel0);
+	teventvoltage->Branch("adc_samples_count_channel1", &adc_samples_count_channel1);
+	teventvoltage->Branch("adc_samples_count_channel2", &adc_samples_count_channel2);
+	teventvoltage->Branch("adc_samples_count_channel3", &adc_samples_count_channel3);
 	teventvoltage->Branch("trigger_pattern", &trigger_pattern);
 	teventvoltage->Branch("trigger_rate", &trigger_rate);
-//	teventvoltage->Branch("clock_tick", &clock_tick);
-//	teventvoltage->Branch("clock_tics_per_second", &clock_tics_per_second);
-//	teventvoltage->Branch("gps_offset", &gps_offset);
-//	teventvoltage->Branch("gps_leap_second", &gps_leap_second);
-//	teventvoltage->Branch("gps_status", &gps_status);
-//	teventvoltage->Branch("gps_alarms", &gps_alarms);
-//	teventvoltage->Branch("gps_warnings", &gps_warnings);
+	teventvoltage->Branch("clock_tick", &clock_tick);
+	teventvoltage->Branch("clock_tics_per_second", &clock_tics_per_second);
+	teventvoltage->Branch("gps_offset", &gps_offset);
+	teventvoltage->Branch("gps_leap_second", &gps_leap_second);
+	teventvoltage->Branch("gps_status", &gps_status);
+	teventvoltage->Branch("gps_alarms", &gps_alarms);
+	teventvoltage->Branch("gps_warnings", &gps_warnings);
 	teventvoltage->Branch("gps_time", &gps_time);
 	teventvoltage->Branch("gps_long", &gps_long);
 	teventvoltage->Branch("gps_lat", &gps_lat);
 	teventvoltage->Branch("gps_alt", &gps_alt);
 	teventvoltage->Branch("gps_temp", &gps_temp);
-//	teventvoltage->Branch("digi_ctrl", &digi_ctrl);
+	teventvoltage->Branch("digi_ctrl", &digi_ctrl);
 	teventvoltage->Branch("digi_prepost_trig_windows", &digi_prepost_trig_windows);
-//	teventvoltage->Branch("channel_properties0", &channel_properties0);
-//	teventvoltage->Branch("channel_properties1", &channel_properties1);
-//	teventvoltage->Branch("channel_properties2", &channel_properties2);
-//	teventvoltage->Branch("channel_properties3", &channel_properties3);
-//	teventvoltage->Branch("channel_trig_settings0", &channel_trig_settings0);
-//	teventvoltage->Branch("channel_trig_settings1", &channel_trig_settings1);
-//	teventvoltage->Branch("channel_trig_settings2", &channel_trig_settings2);
-//	teventvoltage->Branch("channel_trig_settings3", &channel_trig_settings3);
+	teventvoltage->Branch("channel_properties0", &channel_properties0);
+	teventvoltage->Branch("channel_properties1", &channel_properties1);
+	teventvoltage->Branch("channel_properties2", &channel_properties2);
+	teventvoltage->Branch("channel_properties3", &channel_properties3);
+	teventvoltage->Branch("channel_trig_settings0", &channel_trig_settings0);
+	teventvoltage->Branch("channel_trig_settings1", &channel_trig_settings1);
+	teventvoltage->Branch("channel_trig_settings2", &channel_trig_settings2);
+	teventvoltage->Branch("channel_trig_settings3", &channel_trig_settings3);
 	teventvoltage->Branch("ioff", &ioff);
-	teventvoltage->Branch("x", &x);
-	teventvoltage->Branch("y", &y);
-	teventvoltage->Branch("z", &z);
+	teventvoltage->Branch("trace_x", &trace_x);
+	teventvoltage->Branch("trace_y", &trace_y);
+	teventvoltage->Branch("trace_z", &trace_z);
 
 	this->teventvoltage = teventvoltage;
 
@@ -190,7 +187,7 @@ TTree *GRANDEventVoltage::CreateTree()
 
 void GRANDEventVoltage::ClearVectors()
 {
-//	event_id.clear();
+	event_id.clear();
 	du_id.clear();
 	du_seconds.clear();
 	du_nanoseconds.clear();
@@ -202,45 +199,45 @@ void GRANDEventVoltage::ClearVectors()
 	acceleration_x.clear();
 	acceleration_y.clear();
 	acceleration_z.clear();
-//	battery_level.clear();
+	battery_level.clear();
 	// ToDo: Is this the same as event_version for the whole event?
 	firmware_version.clear();
-//	adc_sampling_frequency.clear();
-//	adc_sampling_resolution.clear();
-//	adc_input_channels.clear();
-//	adc_enabled_channels.clear();
-//	adc_samples_count_total.clear();
-//	adc_samples_count_channel0.clear();
-//	adc_samples_count_channel1.clear();
-//	adc_samples_count_channel2.clear();
-//	adc_samples_count_channel3.clear();
+	adc_sampling_frequency.clear();
+	adc_sampling_resolution.clear();
+	adc_input_channels.clear();
+	adc_enabled_channels.clear();
+	adc_samples_count_total.clear();
+	adc_samples_count_channel0.clear();
+	adc_samples_count_channel1.clear();
+	adc_samples_count_channel2.clear();
+	adc_samples_count_channel3.clear();
 	trigger_pattern.clear();
 	trigger_rate.clear();
-//	clock_tick.clear();
-//	clock_tics_per_second.clear();
-//	gps_offset.clear();
-//	gps_leap_second.clear();
-//	gps_status.clear();
-//	gps_alarms.clear();
-//	gps_warnings.clear();
+	clock_tick.clear();
+	clock_tics_per_second.clear();
+	gps_offset.clear();
+	gps_leap_second.clear();
+	gps_status.clear();
+	gps_alarms.clear();
+	gps_warnings.clear();
 	gps_time.clear();
 	gps_long.clear();
 	gps_lat.clear();
 	gps_alt.clear();
 	gps_temp.clear();
-//	digi_ctrl.clear();
+	digi_ctrl.clear();
 	digi_prepost_trig_windows.clear();
-//	channel_properties0.clear();
-//	channel_properties1.clear();
-//	channel_properties2.clear();
-//	channel_properties3.clear();
-//	channel_trig_settings0.clear();
-//	channel_trig_settings1.clear();
-//	channel_trig_settings2.clear();
-//	channel_trig_settings3.clear();
+	channel_properties0.clear();
+	channel_properties1.clear();
+	channel_properties2.clear();
+	channel_properties3.clear();
+	channel_trig_settings0.clear();
+	channel_trig_settings1.clear();
+	channel_trig_settings2.clear();
+	channel_trig_settings3.clear();
 	// ToDo: What is it?
 	ioff.clear();
-	x.clear();
-	y.clear();
-	z.clear();
+	trace_x.clear();
+	trace_y.clear();
+	trace_z.clear();
 }
--- GRANDEventVoltage.h
+++ GRANDEventVoltage.h
@@ -7,7 +7,6 @@
 #define GTOT_GRANDEVENTVOLTAGE_H
 
 #include <vector>
-//#include "scope.h"
 #include "Traces.h"
 #include "TTree.h"
 #include "TTimeStamp.h"
@@ -22,82 +21,127 @@ class GRANDEventVoltage
 public:
 	// Better have local storage for all the variables if one day we don't init from the structure
 	// Common for the whole event
-//	unsigned int event_size;
+	//! Event size
+	unsigned int event_size;
 	//! Run number
 	unsigned int run_number;
+	//! Event in the run number
 	unsigned int event_number;
-	// ?
+	//! Trigger number from T3 maker
 	unsigned int t3_number;
-	// ?
+	//! First detector unit that triggered in the event
 	unsigned int first_du;
+	//! Unix time corresponding to the GPS seconds of the trigger
 	unsigned int time_seconds;
+	//! GPS nanoseconds corresponding to the trigger of the first triggered station
 	unsigned int time_nanoseconds;
+	//! Trigger type 0x1000 10 s trigger and 0x8000 random trigger, else shower
 	unsigned int event_type;
+	//! Event format version of the DAQ
 	unsigned int event_version;
-	//! Number of Digitizer Units in the event - basically the antennas count
+	//! Number of detector units in the event - basically the antennas count
 	unsigned int du_count;
 	// Separate for each DU (antenna) in the event
 	// As number of triggered antennas is variable, we need vectors here
-//	vector<unsigned short> event_id;
+	//! The T3 trigger number
+	vector<unsigned short> event_id;
+	//! Detector unit (antenna) ID
 	vector<unsigned short> du_id;
+	//! Unix time of the trigger for this DU
 	vector<unsigned int> du_seconds;
+	//! Nanoseconds of the trigger for this DU
 	vector<unsigned int> du_nanoseconds;
-//	vector<double> du_time;
-	// ?
+	//! Trigger position in the trace (trigger start = nanoseconds - 2*sample number)
 	vector<unsigned short> trigger_position;
-	// ?
+	//! Same as event_type, but event_type could consist of different triggered DUs
 	vector<unsigned short> trigger_flag;
-	vector<unsigned short> atm_temperature;
-	vector<unsigned short> atm_pressure;
-	vector<unsigned short> atm_humidity;
-	vector<unsigned short> acceleration_x;
-	vector<unsigned short> acceleration_y;
-	vector<unsigned short> acceleration_z;
-//	vector<unsigned short> battery_level;
-	// ToDo: Is this the same as event_version for the whole event?
+	//! Atmospheric temperature (read via I2C)
+	vector<float> atm_temperature;
+	//! Atmospheric pressure
+	vector<float> atm_pressure;
+	//! Atmospheric humidity
+	vector<float> atm_humidity;
+	//! Acceleration of the antenna in X
+	vector<float> acceleration_x;
+	//! Acceleration of the antenna in Y
+	vector<float> acceleration_y;
+	//! Acceleration of the antenna in Z
+	vector<float> acceleration_z;
+	//! Battery voltage
+	vector<float> battery_level;
+	//! Firmware version
 	vector<unsigned short> firmware_version;
-//	vector<unsigned short> adc_sampling_frequency;
-//	vector<unsigned short> adc_sampling_resolution;
-//	vector<unsigned short> adc_input_channels;
-//	vector<unsigned short> adc_enabled_channels;
-//	vector<unsigned short> adc_samples_count_total;
-//	vector<unsigned short> adc_samples_count_channel0;
-//	vector<unsigned short> adc_samples_count_channel1;
-//	vector<unsigned short> adc_samples_count_channel2;
-//	vector<unsigned short> adc_samples_count_channel3;
-	// ?
+	//! ADC sampling frequency in MHz
+	vector<unsigned short> adc_sampling_frequency;
+	//! ADC sampling resolution in bits
+	vector<unsigned short> adc_sampling_resolution;
+	//! ADC input channels - > 16 BIT WORD (4*4 BITS) LOWEST IS CHANNEL 1, HIGHEST CHANNEL 4. FOR EACH CHANNEL IN THE EVENT WE HAVE: 0: ADC1, 1: ADC2, 2:ADC3, 3:ADC4 4:FILTERED ADC1, 5:FILTERED ADC 2, 6:FILTERED ADC3, 7:FILTERED ADC4. ToDo: decode this?
+	vector<unsigned short> adc_input_channels;
+	//! ADC enabled channels - LOWEST 4 BITS STATE WHICH CHANNEL IS READ OUT ToDo: Decode this?
+	vector<unsigned short> adc_enabled_channels;
+	//! ADC samples callected in all channels
+	vector<unsigned short> adc_samples_count_total;
+	//! ADC samples callected in channel 0
+	vector<unsigned short> adc_samples_count_channel0;
+	//! ADC samples callected in channel 1
+	vector<unsigned short> adc_samples_count_channel1;
+	//! ADC samples callected in channel 2
+	vector<unsigned short> adc_samples_count_channel2;
+	//! ADC samples callected in channel 3
+	vector<unsigned short> adc_samples_count_channel3;
+	//! Trigger pattern - which of the trigger sources (more than one may be present) fired to actually the trigger the digitizer - explained in the docs. ToDo: Decode this?
 	vector<unsigned short> trigger_pattern;
-	// ?
+	//! Trigger rate - the number of triggers recorded in the second preceding the event
 	vector<unsigned short> trigger_rate;
-//	vector<unsigned short> clock_tick;
-//	vector<unsigned int> clock_tics_per_second;
-//	vector<float> gps_offset;
-//	vector<unsigned short> gps_leap_second;
-//	vector<unsigned short> gps_status;
-//	vector<unsigned short> gps_alarms;
-//	vector<unsigned short> gps_warnings;
+	//! Clock tick at which the event was triggered (used to calculate the trigger time)
+	vector<unsigned short> clock_tick;
+	//! Clock ticks per second
+	vector<unsigned int> clock_tics_per_second;
+	//! GPS offset - offset between the PPS and the real second (in GPS). ToDo: is it already included in the time calculations?
+	vector<float> gps_offset;
+	//! GPS leap second
+	vector<unsigned short> gps_leap_second;
+	//! GPS status
+	vector<unsigned short> gps_status;
+	//! GPS alarms
+	vector<unsigned short> gps_alarms;
+	//! GPS warnings
+	vector<unsigned short> gps_warnings;
+	//! GPS time
 	vector<unsigned int> gps_time;
-	vector<double> gps_long;
-	vector<double> gps_lat;
-	vector<double> gps_alt;
+	//! Longitude
+	vector<float> gps_long;
+	//! Latitude
+	vector<float> gps_lat;
+	//! Altitude
+	vector<float> gps_alt;
+	//! GPS temperature
 	vector<float> gps_temp;
-//	vector<vector<unsigned short>> digi_ctrl;
-	// ?
+	//! Control parameters - the list of general parameters that can set the mode of operation, select trigger sources and preset the common coincidence read out time window (Digitizer mode parameters in the manual). ToDo: Decode?
+	vector<vector<unsigned short>> digi_ctrl;
+	//! Window parameters - describe Pre Coincidence, Coincidence and Post Coincidence readout windows (Digitizer window parameters in the manual). ToDo: Decode?
 	vector<vector<unsigned short>> digi_prepost_trig_windows;
-//	vector<vector<unsigned short>> channel_properties0;
-//	vector<vector<unsigned short>> channel_properties1;
-//	vector<vector<unsigned short>> channel_properties2;
-//	vector<vector<unsigned short>> channel_properties3;
-//	vector<vector<unsigned short>> channel_trig_settings0;
-//	vector<vector<unsigned short>> channel_trig_settings1;
-//	vector<vector<unsigned short>> channel_trig_settings2;
-//	vector<vector<unsigned short>> channel_trig_settings3;
-	// ToDo: What is it?
-	// ?
+	//! Channel 0 properties - described in Channel property parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_properties0;
+	//! Channel 1 properties - described in Channel property parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_properties1;
+	//! Channel 2 properties - described in Channel property parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_properties2;
+	//! Channel 3 properties - described in Channel property parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_properties3;
+	//! Channel 0 trigger settings - described in Channel trigger parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_trig_settings0;
+	//! Channel 1 trigger settings - described in Channel trigger parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_trig_settings1;
+	//! Channel 2 trigger settings - described in Channel trigger parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_trig_settings2;
+	//! Channel 3 trigger settings - described in Channel trigger parameters in the manual. ToDo: Decode?
+	vector<vector<unsigned short>> channel_trig_settings3;
+	//! ?? What is it? Some kind of the adc trace offset?
 	vector<unsigned short> ioff;
-	vector<vector<float>> x;
-	vector<vector<float>> y;
-	vector<vector<float>> z;
+	vector<vector<float>> trace_x;
+	vector<vector<float>> trace_y;
+	vector<vector<float>> trace_z;
 
 	//! The TTree for holding the data
 	TTree *teventvoltage;
