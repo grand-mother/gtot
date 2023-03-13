5f3d9007083fb5c8ac977df884eb400a2ca2723a
Lech Piotrowski <science@lwp.email>
2023-03-13 17:50:13 +0100

    Cosmetics

--- TRun.h
+++ TRun.h
@@ -17,62 +17,7 @@ class TRun
 	// I assume we don't need getters and setters for these properties, because they are simple values
 public:
 	// Better have local storage for all the variables if one day we don't init from the structure
-/*
-	_type: str = "run"
 
-	_tree_name: str = "trun"
-
-	## Run mode - calibration/test/physics. ToDo: should get enum description for that, but I don't think it exists at the moment
-	_run_mode: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-	## Run's first event
-	_first_event: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-	## First event time
-			_first_event_time: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-	## Run's last event
-	_last_event: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-	## Last event time
-			_last_event_time: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-
-# These are not from the hardware
-	## Data source: detector, simulation, other
-	_data_source: StdString = StdString("detector")
-	## Data generator: gtot (in this case)
-	_data_generator: StdString = StdString("GRANDlib")
-	## Generator version: gtot version (in this case)
-	_data_generator_version: StdString = StdString("0.1.0")
-	## Trigger type 0x1000 10 s trigger and 0x8000 random trigger, else shower
-			_event_type: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-	## Event format version of the DAQ
-	_event_version: np.ndarray = field(default_factory=lambda: np.zeros(1, np.uint32))
-	## Site name
-# _site: StdVectorList("string") = StdVectorList("string")
-	_site: StdString = StdString("")
-	## Site layout
-	_site_layout: StdString = StdString("")
-# ## Site longitude
-# _site_long: np.ndarray = field(default_factory=lambda: np.zeros(1, np.float32))
-# ## Site latitude
-# _site_lat: np.ndarray = field(default_factory=lambda: np.zeros(1, np.float32))
-	## Origin of the coordinate system used for the array
-	_origin_geoid: np.ndarray = field(default_factory=lambda: np.zeros(3, np.float32))
-
-	## Detector unit (antenna) ID
-			_du_id: StdVectorList = field(default_factory=lambda: StdVectorList("int"))
-	## Detector unit (antenna) (lat,lon,alt) position
-			_du_geoid: StdVectorList = field(default_factory=lambda: StdVectorList("vector<float>"))
-	## Detector unit (antenna) (x,y,z) position in site's referential
-	_du_xyz: StdVectorList = field(default_factory=lambda: StdVectorList("vector<float>"))
-	## Detector unit type
-			_du_type: StdVectorList = field(default_factory=lambda: StdVectorList("string"))
-	## Detector unit (antenna) angular tilt
-	_du_tilt: StdVectorList = field(default_factory=lambda: StdVectorList("vector<float>"))
-	## Angular tilt of the ground at the antenna
-	_du_ground_tilt: StdVectorList = field(default_factory=lambda: StdVectorList("vector<float>"))
-	## Detector unit (antenna) nut ID
-	_du_nut: StdVectorList = field(default_factory=lambda: StdVectorList("int"))
-	## Detector unit (antenna) FrontEnd Board ID
-			_du_feb: StdVectorList = field(default_factory=lambda: StdVectorList("int"))
-*/
 	// These are coming from the hardware
 	//! Run number
 	unsigned int run_number;
