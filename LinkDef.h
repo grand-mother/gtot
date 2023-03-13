45dd0ade12587fdd9064981699aaaaae6d95c2c1
Lech Piotrowski <science@lwp.email>
2023-03-13 17:50:30 +0100

    Needed for dictionary generation during compile time

---
+++ LinkDef.h
@@ -0,0 +1,14 @@
+//
+// Created by lewhoo on 13/03/23.
+//
+
+#ifndef GTOT_LINKDEF_H
+#define GTOT_LINKDEF_H
+
+//#ifdef __ROOTCLING__
+#pragma link C++ class vector < vector<unsigned short> > +;
+#pragma link C++ class vector < vector<short> > +;
+#pragma link C++ class vector < vector<float> > +;
+//#endif
+
+#endif //GTOT_LINKDEF_H
